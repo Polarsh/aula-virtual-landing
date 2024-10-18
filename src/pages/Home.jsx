179545f1c3e07a5db9b8915a6ef1289cec48c1a9
Polar <dybsm98@outlook.es>
2024-10-18 16:22:01 -0500

    Cambio de imagen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,9 @@ export default function HomePage() {
       {/* Hero section */}
       <div className='relative isolate overflow-hidden bg-gray-900 pb-16 pt-16 sm:pb-20'>
         <img
-          src='https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply'
+          src='hero.webp'
           alt=''
-          className='absolute inset-0 -z-10 h-full w-full object-cover'
+          className='absolute inset-0 -z-10 h-full w-full object-cover opacity-5'
         />
         <div
           className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
